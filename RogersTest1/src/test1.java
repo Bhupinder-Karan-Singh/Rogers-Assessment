e575d11c68ca6b7fdd18041480494ae173d43b27
Bhupinder Karan Singh <bhupinderkaransingh@gmail.com>
2022-11-16 18:47:34 -0500

    commit

--- RogersTest1/src/test1.java
+++ RogersTest1/src/test1.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
 public class test1 {
+	public static int a = 1, b = 1;
 	// function to print the pyramid triangle with height
 	public static void printTriangle(int height){
-		// loop to print rows with height upto n rows
-        for (int i=0; i<height; i++){
-        	// loop to create space before the first star in a row
-            for (int j=height-i; j>1; j--){
-                System.out.print(" ");
-            }
-            // loop to create the columns according to outer loop
-            for (int j=0; j<=i; j++ ){
-                System.out.print("* ");
-            }
-            System.out.println();
+		if ((int) Math.sqrt(Math.pow((a - (2 * height - 1) * (b - 1) - height), 2))<b) {
+            System.out.print("*");
+        } else {
+            System.out.print(" ");
         }
+        if ((a - (2 * height - 1) * (b - 1)) % (2 * height - 1) == 0) {
+            System.out.print("\n");
+            b++;
+        }
+        if (a++<height * (2 * height - 1))
+        	printTriangle(height);
     }
-
 	public static void main(String[] args) {
-		int height = 5;  
+		int height = 10;  
         printTriangle(height);
 	}
 }
